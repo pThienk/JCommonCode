671503a310a72c6feb345e70b1a4b82d28c24a70
Punnatorn Thienkingkeaw <pthienkingkeaw24@taboracademy.org>
2025-02-01 01:37:16 -0600

    Starting get_slips and CCDF

---
+++ src/CCDF.jl
@@ -0,0 +1,3 @@
+# File dedicated to the construction of CDF, AND CCDF (despite the name)
+# of general (simulated or experimental) data's event "sizes" (to be defined)
+# and their frequency.
---
+++ src/SlipAnalysis.jl
@@ -0,0 +1,3 @@
+# Rewritten code for Dahmen group's slip avalanches data statistical
+# analysis. As with the Python version, the get_slips function accounts
+# for both displacement-based ("SLIP"), and velocity-based ("SLIP rate")
